22f270774d19a7468c6a4bbeeb121acefdfa985b
Jokki <feocomemierdas@gmail.com>
2026-02-06 16:45:26 +0100

    primer_commit

---
+++ vue-project/src/views/Loginview.vue
@@ -0,0 +1,29 @@
+<script>
+
+</script>
+
+<template>
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body class="layout-centered">
+    <section class="auth-container">
+        <h2>Login</h2>
+        <form id="loginform">
+            <label class="form-label">Mail</label>
+            <input type="email" placeholder="..." id="email" class="form-control" required autofocus>
+            <label class="form-label">Contraseña</label>
+            <input type="password" placeholder="..." id="password" class="form-control" required>
+            <input type="submit" value="Ingresar" class="btn btn-primary">
+        </form>
+        <p>¿No tienes una cuenta? <a href="signup.html">Regístrate</a></p>
+    </section>
+    <script src="js/login.js"></script>
+</body>
+</html>
+</template>
---
+++ vue-project/src/views/Registerview.vue
@@ -0,0 +1,32 @@
+<script>
+
+</script>
+
+<template>
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Registro</title>
+</head>
+<body class="layout-centered">
+    <section class="auth-container">
+        <h2>Registro</h2>
+        <form id="signupForm">
+            <label class="form-label">Nombre</label>
+            <input type="text" placeholder="..." id="name" class="form-control" required>
+            <label class="form-label">Mail</label>
+            <input type="email" placeholder="..." id="email" class="form-control" required>
+            <label class="form-label">Contraseña</label>
+            <input type="password" placeholder="..." id="password" class="form-control" required>
+            <input type="submit" value="Registro" class="btn btn-primary">
+        </form>
+        <p>¿Ya tienes una cuenta? <a href="login.html">Login</a></p>
+    </section>
+    <script src="js/signup.js"></script>
+</body>
+</html>
+</template>
